eeabfe8232d1492f7900898a2c777bd2ffeb2cd6
Moris Sirotić <moris.sirotic1@gmail.com>
2023-03-25 18:08:09 +0100

    Name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export const Navbar = () => {
   return (
-    <div className="flex w-full h-20">
-      <div className="text-4xl self-center justify-self-center m-auto font-bold">
-        Bellis
+    <div className="flex w-full h-20 p-4 mb-24">
+      <div className="text-4xl self-center justify-self-center m-auto text-center font-bold">
+        Frizerski Salon <br/> <span className="text-pink-600 text-6xl">Dolores</span> 
       </div>
     </div>
   );
