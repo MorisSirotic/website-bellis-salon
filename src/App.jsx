d9746082c000feea41f88cf38d87ae7fd8db9aee
Moris Sirotić <moris.sirotic1@gmail.com>
2023-03-25 04:20:47 +0100

    Added euror conversion disclaimer

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
 
   const handleContactClick = (e) => {
     myRef.current.scrollIntoView({ behavior: "smooth" });
-  }
+  };
   return (
     <div className="flex flex-col w-full h-full bg-gradient-to-b from-pink-200 to-slate-200 text-slate-800">
       <Navbar />
 
       <div className="mb-4">
-        <Top contactClick={() => handleContactClick()}/>
+        <Top contactClick={() => handleContactClick()} />
       </div>
 
       <div className="block lg:hidden mb-2">
@@ -31,7 +31,9 @@ function App() {
       <div className="flex w-full mt-0 self-center  max-w-3xl">
         <Services />
       </div>
-
+      <div className="flex text-sm self-center text-center p-2">
+        Dvojno iskazivanje cijena prema fiksnom tečaju 1 euro = 7,53450 kuna.
+      </div>
       <div id="contact" className="mt-0" ref={myRef}>
         <Contact />
       </div>
